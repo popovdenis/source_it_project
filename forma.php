38f684f8c104c856e9d127de54cfae3fbf01b929
unknown <anjuce6@gmail.com>
2015-10-30 13:03:27 +0200

    second

--- forma.php
+++ forma.php
@@ -1,16 +1,29 @@
-<?php
+<script type="text/javascript">
+    function valid(form) {
+        var fail = false;
+        var role1 = form.role.value;
+        if (role1 == "" || role1 == " ") {
+            fail = "Заполните пустое поле";
+        }
+        if (fail) {
+            alert(fail);
+            return false;
+        }
+        else {
+            <?php
 include "Role.php";
 $role = new Role;
 $role->setR($role->getRoles('role'));
 if (isset($_POST['role']) && !empty($_POST['role'])) {
     echo $role->postRole('role', $_POST['role']);
-} else {
-    echo '<center><b>' . "Заполните поле" . '</b></center>';
 }
 ?>
+        }
+    }
+</script>
 <body bgcolor="#ffe4b5">
 <center>
-    <form action="" method="post">
+    <form action="" method="post" onsubmit="valid(this)">
         Role:<input name="role" type="text">
         <input name="add" type="submit" value="Добавить">
     </form>
