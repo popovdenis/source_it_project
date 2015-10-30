b2b9a9738b2accd884e4721bd982aab6ec86e137
unknown <qaweeds@gmail.com>
2015-10-30 13:35:49 +0200

    'del_ajax'

--- users.php
+++ users.php
@@ -27,8 +27,6 @@ if (isset($_GET['reduser']) and !empty($_GET['reduser'])) {
     $user_info = $users->getUser($id);
     $_SESSION['user'] = $user_info;
     header("Location: red_user.php");
-
-
 }
 if (isset($_GET['deluser']) and !empty($_GET['deluser'])) {
     $id = $_GET['deluser'];
@@ -40,8 +38,21 @@ if (isset($_GET['deluser']) and !empty($_GET['deluser'])) {
 <html>
 <head>
     <title>Список пользователей</title>
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery-1.11.3.js"></script>
+    <style type="text/css">
+        .wait-bar{
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            width: 64px;
+            height: 64px;
+        }
+    </style>
 </head>
 <body>
+<div class='content'>
+<div class='wait-bar'></div>
 <a href="index.php">Добавить нового пользователя</a>
 <table border="1" width="100%">
     <tr>
@@ -74,5 +85,35 @@ if (isset($_GET['deluser']) and !empty($_GET['deluser'])) {
     }
     ?>
 </table>
+</div>
 </body>
 </html>
+<script type="text/javascript">
+
+$('table').find('a').click(function(event){
+    $q = $(this).attr('href');
+    if(!$q.indexOf('?deluser')){
+         event.preventDefault();
+        $url = "users.php" + $q;
+    $.ajax({
+        url: $url,
+        beforeSend: function(){
+            $('.wait-bar').css('background-image', 'url(./images/load.gif)');
+        },
+        error: function(){
+            alert('AJAX ERROR');
+        },
+        success: function(data) {
+            $('.wait-bar').css('background-image', 'none');
+            $('body').html(data);
+        }
+    });
+    }else{
+        console.log('red');
+    }
+})
+
+
+
+
+</script>
